d8fbb5c3c68e98a9b9f87253643063754ca56d23
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-14 08:32:56 +0530

    Add files via upload
    C Program To Compute The Perimeter and Area of A Rectangle.

---
+++ Introduction/rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int l=7,b=5,per,area;
+	per=2*(l+b);
+	area=l*b;
+	printf("Perimeter Of The Rectangle =%d inches",per);
+    printf("\nArea Of The Rectangle =%d Sqyare inches",area);	
+    return 0;
+}
