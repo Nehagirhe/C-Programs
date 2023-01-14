1b63478a6f47f39d087322a190dda10b4a8ce21b
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-14 09:16:37 +0530

    Add files via upload
    C Program That Accepts An Employee's ID ,total Worked Hours Of A Month And The Amount He Received Per Hour.Print The Employee's ID
    And Salary Of A Particular Month.

---
+++ Introduction/Employee.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int empid=0342;
+	int hrs=8,sal=15000,tot;
+	tot=sal*hrs;
+	printf("EmployeeId = 0342");
+	printf("\n Salary=%d\n",tot);
+    return 0;
+}
