11e12c8a6dd1b02d5c794a6bda46c5f987575c34
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-14 08:40:45 +0530

    Add files via upload
    C Program To Compute The Perimeter And Area Of A Circle With A Radius Of 6 Inches.

---
+++ Introduction/circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int r=6;
+	float pi=3.14,per,area;
+	per=2*pi*r;
+	area=pi*r*r;
+	printf("Perimeter Of The Circle =%f ",per);
+    printf("\nArea Of The Circle =%f ",area);	
+    return 0;
+}
