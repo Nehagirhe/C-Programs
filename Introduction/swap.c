59a04ad2744e1a3ffa80b89c2d7997dad8aeae71
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-15 12:40:45 +0530

    Add files via upload
    C Program To Swap Of Two Numbers.

---
+++ Introduction/swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) {
+int a=10,b=20;
+a=a-b;
+b=a+b;
+a=b-a;
+printf("a=%d\n",a);
+printf("b=%d\n",b);
+return 0;
+}
