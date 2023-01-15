9ee82cb2dc4b86386eee4ee9407d7f6ddd366146
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-15 12:02:03 +0530

    Add files via upload
    C Program To Calculate A Bike's Average Consumption From The Given Total Distance Traveled(in km) And Spent Fuel.

---
+++ Introduction/bike's average.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) {
+int dist=350,fuel=5;
+float avg;avg=dist/fuel;
+printf("Average Consumption %f (km/lt)\n",avg);
+	return 0;
+}
