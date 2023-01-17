18e9d94b1287240a29a1bed779f83a2261341a2f
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-17 08:23:59 +0530

    Update sphere.c

--- Input-Output/sphere.c
+++ Input-Output/sphere.c
@@ -4,9 +4,9 @@
 int main()
 {
 float pi=3.14,volume,r;
-printf("input the radius of the sphere :");   //2.56
+printf("input the radius of the sphere :");   
 scanf("%f",&r);
-volume=1.33*pi*r*r*r;                        //70.065010
+volume=1.33*pi*r*r*r;                        
 printf("the volume of sphere is :%f",volume);
 return 0;	
 }
