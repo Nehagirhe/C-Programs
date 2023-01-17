6570ccfcb88427844fd25b53efb3bf072675d031
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-17 08:22:13 +0530

    Add files via upload
    C Program That Calculates The Volume Of A Sphere.

---
+++ Input-Output/sphere.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+float pi=3.14,volume,r;
+printf("input the radius of the sphere :");   //2.56
+scanf("%f",&r);
+volume=1.33*pi*r*r*r;                        //70.065010
+printf("the volume of sphere is :%f",volume);
+return 0;	
+}
