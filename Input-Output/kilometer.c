f17f7bce9b97a98b8387f1c85ec9c19eb363eacf
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-18 08:13:22 +0530

    Add files via upload

---
+++ Input-Output/kilometer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dist,meter,feet,inch,cm;
+	printf("Enter distance in kilometers:");   //2
+	scanf("%d",&dist);        
+	meter =dist*1000;                        //2000
+	feet=dist*3280;                          //6560
+	inch=dist*39370;                         //78740
+	cm=dist*100000;                         //200000
+	printf("kilometers To Meter: %d\n",meter);
+	printf("Kilometers To Feet:%d \n",feet);
+	printf("Kilometers To Inch:%d \n",inch);
+	printf("Kilometers To Centimeters:%d \n",cm);
+	return 0;
+}
