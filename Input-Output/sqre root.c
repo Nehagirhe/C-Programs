95ca7d2b0cc7aad229efd9fc5ea38e1251c51613
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-18 08:09:22 +0530

    Add files via upload
    C Program That Calculates the Square Root of A number.

---
+++ Input-Output/sqre root.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+	int n,i;
+	printf("enter the value of n:");    //25
+	scanf("%d",&n);
+	i=sqrt(n);                          //5
+	printf("square root of %d is:%d",n,i);
+}
