e6e33fc7727776a3b9f86727f214d3fe1e9888a7
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-19 07:24:51 +0530

    Add files via upload
    program to find the area of the square.

---
+++ Input-Output/square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int main()
+{
+	int area,n,s;
+	printf("enter the side of a sqyare :");    
+	scanf("%d",&s);
+	area=s*s;                      
+	printf("Area of a square is:%d",area);
+}
