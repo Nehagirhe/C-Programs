8b52b07019ecdf9e23a68f48b6935c7821e3ece3
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-19 07:28:16 +0530

    Update square.c

--- Input-Output/square.c
+++ Input-Output/square.c
@@ -1,3 +1,5 @@
+//Program To Find The Area Of The Square.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include<math.h>
@@ -5,8 +7,8 @@
 int main()
 {
 	int area,n,s;
-	printf("enter the side of a square :");    
+	printf("enter the side of a square :");    //2
 	scanf("%d",&s);
-	area=s*s;                      
-	printf("Area of a square is:%d",area);
+	area=s*s;                                     //2*2
+	printf("Area of a square is:%d",area);        //4
 }
