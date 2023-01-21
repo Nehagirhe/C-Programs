4b1b988fd170d121463ba85c0309ff06696173fb
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-21 07:11:44 +0530

    Add files via upload

---
+++ Loops/multiplication table.c
@@ -0,0 +1,15 @@
+//C Program To Display The Multiplication TableOf A Given Integer.
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    int a=15,b;
+	for(b=1;b<=10;b++)
+	{
+	printf(" %d X %d =%d\n",a,b,a*b);
+	}
+	
+	return 0;
+}
