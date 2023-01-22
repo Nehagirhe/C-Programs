bfd4b0547b13285cf9ebeb53cb338436d6761edb
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-22 07:48:37 +0530

    Add files via upload

---
+++ Loops/odd.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    int a,sum=0;
+    for(a=1;a<=20;a+=2)
+    {
+    	printf("%d ",a);
+    	sum=sum+a;
+}
+printf("\nThe Sum Of Odd Natural Number Upto 10 Terms:%d",sum);
+	return 0;
+}
