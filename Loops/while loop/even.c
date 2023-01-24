dfa83e8d6dadf7b71ddaad351869842c37cd1cf2
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-24 08:04:06 +0530

    Add files via upload

---
+++ Loops/while loop/even.c
@@ -0,0 +1,18 @@
+//C Program To Print Even Numbers
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i,n;
+	printf("Enter A Number: ");
+	scanf("%d",&n);
+	i=2;
+	while(i<=n)
+	{
+		printf("%d ",i);
+		i=i+2;
+	}
+	return 0;
+}
