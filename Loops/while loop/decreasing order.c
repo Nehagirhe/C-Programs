dc06146413edfd5f385cf9434b2dd9b887e00ef5
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-24 07:55:36 +0530

    Add files via upload

---
+++ Loops/while loop/decreasing order.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i=10;
+	while(i>=1)
+	{
+		printf("%d ",i);
+		i--;
+	}
+	return 0;
+}
