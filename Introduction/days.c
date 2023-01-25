00d7b4dd34e7fdbf00dfad9ef1edcf9009f1c40d
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-25 07:59:21 +0530

    Update days.c

--- Introduction/days.c
+++ Introduction/days.c
@@ -6,10 +6,10 @@ int main()
 	int totaldays=1329,totalremdays=0,year=0,weeks=0,days=0,months=0;
 	year=totaldays/365;
 	printf("\nyears: %d",year);
-	totalremdays=totaldays-year*365;
-	weeks=totalremdays/7;
-	printf("\nWeeks: %d",weeks);
-	days=totalremdays-weeks*7;
+	totalremdays=totaldays-year*365;     //1329-3*365=234
+	weeks=totalremdays/7;                //234/7=33
+	printf("\nWeeks: %d",weeks);      
+	days=totalremdays-weeks*7;           //234-33*7=3
 	printf("\nDays: %d",days);
     return 0;
 }
