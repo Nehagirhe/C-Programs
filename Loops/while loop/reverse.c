07f753ea46fcd618e9ea0147b4a968b458670c8f
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-27 12:14:45 +0530

    Add files via upload

---
+++ Loops/while loop/reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int n,rev=0;
+	printf("enter the number:");     // 123
+	scanf("%d",&n);
+	while(n!=0){                    
+	rev=rev*10+n%10;             // 0=0*10+123%10= 3             3*10+12%10=32               
+	n=n/10;}                    //123=123/10=12                  32=32/10=2                 
+	printf("reverse=%d",rev);
+	return 0;
+}
