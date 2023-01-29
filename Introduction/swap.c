9d45552c4876200378d97f6c3e6e559e8df8ddf4
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-29 08:32:14 +0530

    Update swap.c

--- Introduction/swap.c
+++ Introduction/swap.c
@@ -1,8 +1,7 @@
 //C Program To Swap Two Numbers.
 #include <stdio.h>
 #include <stdlib.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(int argc, char *argv[]) {
+int main {
 int a=10,b=20;
 a=a-b;
 b=a+b;
