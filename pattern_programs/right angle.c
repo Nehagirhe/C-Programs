63024ae1fe0226cffa978aed531326b420a078c4
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-02-08 08:01:23 +0530

    Update right angle.c

--- pattern_programs/right angle.c
+++ pattern_programs/right angle.c
@@ -1,4 +1,4 @@
-// Program In C To Display The Pattern Like Right Angle Triangle Using An Asterisk'*'.
+// Program In C To Display The Pattern Like Right Angle Triangle Using An Asterisk '*'.
 #include <stdio.h>
 #include <stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
