b069620a6f87acf0fd79096e0031421ec5c72e03
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-02-13 08:03:15 +0530

    Add files via upload

---
+++ Array/array1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+   int main()
+{  
+	int a[10]={1,1,2,3,4,5,6,7,8,9};
+	int i;
+	printf("elements in the array are :");
+	for(i=0;i<10;i++)
+	{
+		printf("%d  ",a[i]);
+	}
+			
+	return 0;
+}
