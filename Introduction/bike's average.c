2d18fee03a70f4edcf6df1dc9c450791ff6b8921
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-02-17 08:43:56 +0530

    Update bike's average.c

--- Introduction/bike's average.c
+++ Introduction/bike's average.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 int dist=350,fuel=5;
 float avg;avg=dist/fuel;
 printf("Average Consumption %f (km/lt)\n",avg);
