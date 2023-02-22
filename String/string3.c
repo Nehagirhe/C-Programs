3265ff5454673c54426acd530e0516d2bee3acd6
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-02-22 08:09:31 +0530

    Add files via upload

---
+++ String/string3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char a[13];//="w3resource";
+	int i;
+	//int n=sizeofa[]/a(10);
+	printf("the character of the strings are:");
+		for(i=13;i>0;i--)
+	{
+		
+		scanf("%c",&a[i]);
+		printf("%c ",a[i]);
+	}
+	strrev(str); 
+	printf("%s ",str);
+	return 0;
+}
