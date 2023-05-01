9d1a82a3973d2ab377dfed6f9246756ec5c5ff57
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-05-01 21:24:21 +0530

    Update array 6.c

--- Array/array 6.c
+++ Array/array 6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(int argc, char *argv[]) {
 int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
